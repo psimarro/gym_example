3b6f609df95451ec46f26860c670222671e1eef8
pedro <simarroguerrapedro@gmail.com>
2020-12-03 20:13:42 +0100

    cambios entorno

--- gym-example/gym_example/envs/kvazaar_env.py
+++ gym-example/gym_example/envs/kvazaar_env.py
@@ -28,11 +28,23 @@ class Kvazaar_v0 (gym.Env):
         #el espacio de observaciones es un rango de floats de 0 a 200
         self.observation_space = Box(low=np.array([0]), high=np.array([200]), dtype=np.float32)
         self.goal = 0 #no hay objetivo de momento
+        self.kvazaar = None
 
         self.seed() #generamos semilla de randoms
         # self.reset() #generamos la primera observacion
     
     def reset(self):
+        self.reset_kvazaar()
+        
+        self.count = 0
+        # self.state = {"intervalo": 0, "fps" :0} #el estado del entorno en un momento dado es el número de frames devueltos por kvazaar para un bloque 
+        self.state = [0]
+        self.reward = 0 #la recompensa inicial es 0
+        self.done = False
+        self.info = {"estado": "inicio"}
+        return self.state
+
+    def reset_kvazaar(self):
         comando = [self.kvazaar_path, 
                    "--input", self.vid_path, 
                    "--output", "/dev/null", 
@@ -41,24 +53,18 @@ class Kvazaar_v0 (gym.Env):
                    "--threads=" + str(self.nCores)]
 
         # creamos subproceso de kvazaar
-        if self.kvazaar is None or self.kvazaar.poll():
-            self.kvazaar = subprocess.Popen(comando, 
+        
+        self.kvazaar = subprocess.Popen(comando, 
                                         stdin=subprocess.PIPE, 
                                         stdout=subprocess.PIPE, 
                                         universal_newlines=True, bufsize=1, 
                                         env={'NUM_FRAMES_PER_BATCH': '24'})
-        
-        
-        self.count = 0
-        # self.state = {"intervalo": 0, "fps" :0} #el estado del entorno en un momento dado es el número de frames devueltos por kvazaar para un bloque 
-        self.state = [0]
-        self.reward = 0 #la recompensa inicial es 0
-        self.done = False
-        self.info = {"inicio"}
-        return self.state
-
+    
     def step(self, action):
         assert self.action_space.contains(action)
+        
+        if self.done:
+            self.reset_kvazaar()
         
         action += 1 #ya que el espacio va de 0 a nCores-1
         
@@ -70,8 +76,8 @@ class Kvazaar_v0 (gym.Env):
         
         self.calculate_state(output=output)
        
-        self.info = {output.strip()}
-        if self.info != {'END'}:
+        self.info["estado"] = output.strip()
+        if self.info["estado"] == 'END':
             self.count += 1
 
         try:
@@ -82,7 +88,7 @@ class Kvazaar_v0 (gym.Env):
         return [self.state, self.calculate_reward(), self.done, self.info]
     
     def calculate_reward(self):
-        if self.info == {'END'}:
+        if self.info["estado"] == 'END':
             self.reward = self.REWARD_NEGATIVE
         elif self.state[0] < 24:
             self.reward = self.REWARD_NEGATIVE
@@ -94,7 +100,7 @@ class Kvazaar_v0 (gym.Env):
     def calculate_state(self, output):
         if(output == "END"):
             self.done = True
-            self.info = {'END'}
+            self.info["estado"] = 'END'
         else:
             ## eliminamos la primera parte de la salida ("FPS:") y la guardamos en el nuevo estado
             output_value = np.float32(output[4:])
@@ -105,7 +111,7 @@ class Kvazaar_v0 (gym.Env):
 
 
     def render(self, mode="human"):
-        if self.info == {'END'}:
+        if self.info["estado"] == 'END':
             print (self.info)
         else:
             print((self.state[0]), self.reward)

82134cef8c3f4eaee2d7fa1bb75e77f2903b1423
Pedro Simarro <simarroguerrapedro@gmail.com>
2020-12-09 20:41:27 +0100

    cambios entorno: formatos salida, información mejorados asegurado kvazaar siempre corriendo añadido metodo close que cierra kvazaar

--- gym-example/gym_example/envs/kvazaar_env.py
+++ gym-example/gym_example/envs/kvazaar_env.py
@@ -1,13 +1,13 @@
 import gym
 from gym.utils import seeding
-from gym.spaces import Dict, Box, Discrete
+from gym.spaces import Dict, Box, Discrete, MultiDiscrete
 import numpy as np
 import subprocess
 import bisect
+import time
 
 class Kvazaar_v0 (gym.Env):
     
-    # De momento estas constantes no son muy útiles
     MAX_STEPS = 10
     REWARD_POSITIVE = 1
     REWARD_NEGATIVE = 0
@@ -21,7 +21,6 @@ class Kvazaar_v0 (gym.Env):
         self.kvazaar_path = kwargs.get("kvazaar_path")
         self.vid_path = kwargs.get("vid_path")
         self.nCores = kwargs.get("nCores")
-        self.intervalos = kwargs.get("intervalos")
         
         self.action_space = Discrete(self.nCores)
         #El espacio de acciones corresponde a los cores, de 0 a nCores-1
@@ -35,13 +34,11 @@ class Kvazaar_v0 (gym.Env):
     
     def reset(self):
         self.reset_kvazaar()
-        
         self.count = 0
-        # self.state = {"intervalo": 0, "fps" :0} #el estado del entorno en un momento dado es el número de frames devueltos por kvazaar para un bloque 
         self.state = [0]
         self.reward = 0 #la recompensa inicial es 0
         self.done = False
-        self.info = {"estado": "inicio"}
+        self.info = {"estado": "running", "intervalo": 0}
         return self.state
 
     def reset_kvazaar(self):
@@ -54,21 +51,19 @@ class Kvazaar_v0 (gym.Env):
 
         # creamos subproceso de kvazaar
         
-        if self.kvazaar is None or self.kvazaar.poll() is not None:
-            self.kvazaar = subprocess.Popen(comando, 
+        self.kvazaar = subprocess.Popen(comando, 
                                         stdin=subprocess.PIPE, 
                                         stdout=subprocess.PIPE, 
                                         universal_newlines=True, bufsize=1, 
                                         env={'NUM_FRAMES_PER_BATCH': '24'})
-    
-    def step(self, action):
-        
-        # if self.done:
-        #     #self.reset_kvazaar()
-        #     pass
-        # else:
 
-        
+        while not self.kvazaar:
+            time.sleep(1)
+
+    def step(self, action):
+        if self.info["estado"] == 'END':
+            self.reset_kvazaar()
+
         assert self.action_space.contains(action)
         action += 1 #ya que el espacio va de 0 a nCores-1
         
@@ -93,11 +88,8 @@ class Kvazaar_v0 (gym.Env):
     
     def calculate_reward(self):
         if self.info["estado"] == 'END':
-            self.reward = self.REWARD_NEGATIVE
-        elif self.state[0] < 24:
-            self.reward = self.REWARD_NEGATIVE
-        else:
             self.reward = self.REWARD_POSITIVE
+        else: self.reward = 0 if(self.state[0] < 24) else 1
 
         return self.reward
 
@@ -108,22 +100,21 @@ class Kvazaar_v0 (gym.Env):
         else:
             ## eliminamos la primera parte de la salida ("FPS:") y la guardamos en el nuevo estado
             output_value = np.float32(output[4:])
+            self.info["intervalo"] = 0 if (output_value < 24) else 1
             self.state = [output_value]
-            # self.state["intervalo"] = bisect.bisect_left(self.intervalos, output_value)
-            # self.state["fps"] = output_value
 
 
 
     def render(self, mode="human"):
         if self.info["estado"] == 'END':
-            print (self.info)
+            print (self.info["estado"])
         else:
-            print((self.state[0]), self.reward)
-
+            l = '{:>7.1f}  box:{:>1}  reward:{:<10}'.format(self.state[0], self.info["intervalo"], self.reward)
+            print(l)
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
         
     def close(self):
-        pass
+       self.kvazaar.kill()
